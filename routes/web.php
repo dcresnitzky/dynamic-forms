90d5715185354aa6099403d9c9428f038fc0a108
Daniel Resnitzky <daniel.resnitzky@veus.com.br>
2022-09-21 17:56:43 -0300

    adjust ident

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ Route::get('/', [App\Http\Controllers\BuilderController::class, 'create']);
 Route::post('/store', [App\Http\Controllers\BuilderController::class, 'store'])->name('store');
 Route::post('/validate', [App\Http\Controllers\FormController::class, 'store'])->name('validate');
 
-Route::prefix('dynamic-forms')->name('dynamic-forms.')->group(function () {
-    // Dummy route so we can use the route() helper to give formiojs the base path for this group
-    Route::get('/')->name('index');
-    Route::get('form', [App\Http\Controllers\DynamicFormsResourceController::class, 'index']);
-    Route::get('form/{resource}', [App\Http\Controllers\DynamicFormsResourceController::class, 'resource']);
-    Route::get('form/{resource}/submission', [App\Http\Controllers\DynamicFormsResourceController::class, 'resourceSubmissions']);
+Route::prefix('dynamic-forms')
+    ->name('dynamic-forms.')->group(function () {
+        // Dummy route so we can use the route() helper to give formiojs the base path for this group
+        Route::get('/')->name('index');
+        Route::get('form', [App\Http\Controllers\DynamicFormsResourceController::class, 'index']);
+        Route::get('form/{resource}', [App\Http\Controllers\DynamicFormsResourceController::class, 'resource']);
+        Route::get('form/{resource}/submission', [App\Http\Controllers\DynamicFormsResourceController::class, 'resourceSubmissions']);
 });
